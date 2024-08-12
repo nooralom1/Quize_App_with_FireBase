77ccec717771f5e3553d1c768d876b10af4ae2a7
NS NooR <non377938@gmail.com>
2024-08-12 15:24:13 +0600

    guess the logo

--- lib/views/screens/guess_thr_logo/guess_the_logo.dart
+++ lib/views/screens/guess_thr_logo/guess_the_logo.dart
@@ -14,9 +14,9 @@ class _GuessTheLogoPageState extends State<GuessTheLogoPage> {
       backgroundColor: const Color(0xff4775d1),
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Stack(
+          child: Column(
             children: [
-              Column(
+              Stack(
                 children: [
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -48,8 +48,8 @@ class _GuessTheLogoPageState extends State<GuessTheLogoPage> {
                               height: 5,
                             ),
                             Text("Lv. 1",
-                                style:
-                                    TextStyle(color: Colors.white70, fontSize: 20)),
+                                style: TextStyle(
+                                    color: Colors.white70, fontSize: 20)),
                           ],
                         ),
                         Container(
@@ -69,7 +69,8 @@ class _GuessTheLogoPageState extends State<GuessTheLogoPage> {
                                 ),
                                 Text(
                                   "44",
-                                  style: TextStyle(color: Colors.white, fontSize: 18),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 18),
                                 ),
                                 Icon(
                                   Icons.light_mode_outlined,
@@ -83,39 +84,160 @@ class _GuessTheLogoPageState extends State<GuessTheLogoPage> {
                       ],
                     ),
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    height: 200,
-                    width: 300,
-                    child: Card(
-                      color: Colors.white,
-                      child: Center(
-                        child: Image.asset("assets/images/logo.png"),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 90, left: 50),
+                    child: SizedBox(
+                      height: 200,
+                      width: 300,
+                      child: Card(
+                        color: Colors.white,
+                        child: Center(
+                          child: Image.asset("assets/images/logo.png"),
+                        ),
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 50,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 350, left: 15),
+                    child: SizedBox(
+                      height: 45,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        child: ListView.builder(
+                          itemCount: 8,
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return const SizedBox(
+                              width: 42,
+                              child: Card(
+                                color: Color(0xff6f94dc),
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                      border: OutlineInputBorder(
+                                          borderSide: BorderSide.none)),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ),
                   ),
-                  SizedBox(
-                    height: 45,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 400, left: 170),
+                    child: SizedBox(
+                      height: 65,
+                      width: 65,
+                      child: Card(
+                        color: Color(0xff2851a4),
+                        child: Center(
+                          child: Text(
+                            "i",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 577),
+                    child: Container(
+                      height: 250,
+                      width: 400,
+                      decoration: const BoxDecoration(
+                          color: Color(0xff6f94dc),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(50),
+                              topRight: Radius.circular(50))),
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 535, left: 40),
+                    child: SizedBox(
+                      height: 100,
+                      width: 320,
+                      child: Card(
+                        color: Colors.white,
+                        child: Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SizedBox(
+                                height: 65,
+                                width: 65,
+                                child: Card(
+                                  color: Color(0xff2851a4),
+                                  child: Center(
+                                      child: Icon(
+                                    Icons.share,
+                                    color: Colors.white,
+                                    size: 35,
+                                  )),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              SizedBox(
+                                height: 65,
+                                width: 65,
+                                child: Card(
+                                  color: Color(0xff2851a4),
+                                  child: Center(
+                                      child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                    size: 35,
+                                  )),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              SizedBox(
+                                height: 65,
+                                width: 65,
+                                child: Card(
+                                  color: Color(0xff2851a4),
+                                  child: Center(
+                                      child: Icon(
+                                    Icons.star_border,
+                                    color: Colors.white,
+                                    size: 35,
+                                  )),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 660),
+                    child: SizedBox(
+                      height: 60,
                       child: ListView.builder(
                         itemCount: 8,
                         scrollDirection: Axis.horizontal,
                         shrinkWrap: true,
                         itemBuilder: (context, index) {
-                          return SizedBox(
-                            width: 42,
+                          return const SizedBox(
+                            width: 49.6,
                             child: Card(
-                              color: Color(0xff6f94dc),
-                              child: TextField(
-                                decoration: InputDecoration(
-                                    border: OutlineInputBorder(
-                                        borderSide: BorderSide.none)),
+                              color: Colors.white,
+                              child: Center(
+                                child: Text(
+                                  "A",
+                                  style: TextStyle(
+                                      color: Colors.deepPurple,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 35),
+                                ),
                               ),
                             ),
                           );
@@ -123,37 +245,34 @@ class _GuessTheLogoPageState extends State<GuessTheLogoPage> {
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  SizedBox(
-                    height: 65,
-                    width: 65,
-                    child: Card(
-                      color: Color(0xff2851a4),
-                      child: Center(
-                        child: Text(
-                          "i",
-                          style: TextStyle(
+                  Padding(
+                    padding: const EdgeInsets.only(top: 720),
+                    child: SizedBox(
+                      height: 60,
+                      child: ListView.builder(
+                        itemCount: 8,
+                        scrollDirection: Axis.horizontal,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return const SizedBox(
+                            width: 49.6,
+                            child: Card(
                               color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold),
-                        ),
+                              child: Center(
+                                child: Text(
+                                  "A",
+                                  style: TextStyle(
+                                      color: Colors.deepPurple,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 35),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 106,
-                  ),
-                  Container(
-                    height: 250,
-                    width: 400,
-                    decoration: BoxDecoration(
-                        color: Color(0xff6f94dc),
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            topRight: Radius.circular(50))),
-                  )
                 ],
               ),
             ],
