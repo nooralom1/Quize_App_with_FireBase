3239ff22ab50bda4edac866686a7db2d15cb6f56
NS NooR <non377938@gmail.com>
2024-08-13 21:11:09 +0600

    models

---
+++ lib/models/all_models.dart
@@ -0,0 +1,24 @@
+
+class Letters{
+
+  final String letter;
+  Letters({required this.letter});
+}
+List<Letters> letter = [
+  Letters(letter: "K"),
+  Letters(letter: "C"),
+  Letters(letter: "N"),
+  Letters(letter: "K"),
+  Letters(letter: "X"),
+  Letters(letter: "A"),
+  Letters(letter: "N"),
+  Letters(letter: "G"),
+  Letters(letter: "I"),
+  Letters(letter: "B"),
+  Letters(letter: "F"),
+  Letters(letter: "F"),
+  Letters(letter: "K"),
+  Letters(letter: "K"),
+  Letters(letter: "X"),
+  Letters(letter: "S"),
+];
