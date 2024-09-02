18767f1668e35a78a2e1732b0f72a2ada9c81b1d
NS NooR <non377938@gmail.com>
2024-09-02 19:15:16 +0600

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
-
 import 'package:flutter/material.dart';
 import 'package:quize_app/views/screens/mypage/mypage.dart';
 
-void main(){
-  runApp( const MyApp());
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
